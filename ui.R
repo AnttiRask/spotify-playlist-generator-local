55ce712d91a2a19922ce05efe80e6ce66d28a78a
Antti Rask <antti.rask@teosto.fi>
2023-07-16 16:12:16 +0300

    -Adding more better comments

--- ui.R
+++ ui.R
@@ -1,4 +1,4 @@
-# Load packages
+# Load required packages
 library(conflicted)
 library(plotly)
 library(purrr)
@@ -7,13 +7,19 @@ library(shinythemes)
 library(stringr)
 library(waiter)
 
+# Load scripts from the 'scripts' folder
 source("scripts/global.R", local = TRUE)
 
-# Define UI
+# Define the UI function
 fluidPage(
+    
+    # Automatically display a loading screen until UI is ready
     autoWaiter(),
+    
+    # Set the theme and custom CSS
     theme = shinytheme("cyborg"),
     includeCSS("css/styles.css"),
+    
     navbarPage(
         
         # Application title
@@ -31,15 +37,23 @@ fluidPage(
             sidebarPanel(
                 h3("Input:"),
                 br(),
+                
+                # User inputs for Spotify Developer credentials
                 textInput("client_id", "Client ID: ", ""),
                 passwordInput("client_secret", "Client Secret: ", ""),
                 br(),
+                
+                # Button to validate the user's credentials
                 actionButton("btn", "Validate"),
                 br(),
                 br(),
+                
+                # Output message after validation
                 textOutput("validate_message")
             ),
             mainPanel(
+                
+                # Display welcome and instructions
                 h2("Welcome to the Spotify Playlist Generator"),
                 br(),
                 h6("You can use this tool to see different analyses on your favorite music on Spotify. You can even create new playlists that use your favorites as a starting point!"),
@@ -59,14 +73,20 @@ fluidPage(
             "Feature per Album",
             sidebarPanel(
                 h3("Features:"),
+                
+                # User selection of features to display
                 selectInput(
                     "feature",
                     "Select a feature to view:",
                     choices  = features,
                     selected = "acousticness"
                 ),
+                
+                # Introduction about the selected feature
                 textOutput("feature_introduction")
             ),
+            
+            # Plotting area
             mainPanel(
                 plotlyOutput("summary_plot", height = 700)
             )
@@ -77,6 +97,8 @@ fluidPage(
             "Average Features",
             div(
                 class = "plot-center",
+                
+                # Plotting area
                 mainPanel(plotOutput("artists_plot", height = 700)
                 )
             )
@@ -87,6 +109,8 @@ fluidPage(
             "Mood Quadrants",
             sidebarPanel(
                 h3("Features:"),
+                
+                # User selection for X and Y axis features
                 selectInput(
                     "x_var",
                     "X Axis (Horizontal):",
@@ -100,6 +124,8 @@ fluidPage(
                     selected = "valence"
                 )
             ),
+            
+            # Plotting area
             mainPanel(plotlyOutput("tracks_plot", height = 700)
             )
         ),
@@ -118,25 +144,35 @@ fluidPage(
                            br()
                     ),
                     column(3,
+                           
+                           # User inputs for Spotify username, number of top artists, and playlist name
                            textInput("user_id", "Spotify Username: ", ""),
                            numericInput("num_top_artists", "Number of top artists (1-5):", min = 1, max = 5, value = 5),
                            textInput("playlist_name", "Playlist Name: "),
                            br(),
+                           
+                           # Button to generate the playlist
                            actionButton("generate", "Generate Playlist")
                     ),
                     column(4,
-                           offset = 1,
+                           # offset = 1,
+                           
+                           # User inputs for target feature values
                            sliderInput("acousticness", "Acousticness (0-1):", min = 0, max = 1, value = 0.5, step = 0.1),
                            sliderInput("danceability", "Danceability (0-1):", min = 0, max = 1, value = 0.5, step = 0.1),
                            sliderInput("energy", "Energy (0-1):", min = 0, max = 1, value = 0.5, step = 0.1)
                     ),
                     column(4,
+                           
+                           # User inputs for target feature values
                            sliderInput("instrumentalness", "Instrumentalness (0-1):", min = 0, max = 1, value = 0.5, step = 0.1),
                            sliderInput("speechiness", "Speechiness (0-1):", min = 0, max = 1, value = 0.5, step = 0.1),
                            sliderInput("valence", "Valence (0-1):", min = 0, max = 1, value = 0.5, step = 0.1)
                     ),
                     column(12,
                            br(),
+                           
+                           # Display the link to the generated playlist
                            uiOutput("playlist_link")
                     )
                 )
